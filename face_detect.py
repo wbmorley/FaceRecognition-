eba2ea613f7588499daaaaf75546b86da84f878b
wbmorley <43530745+wbmorley@users.noreply.github.com>
2021-08-05 17:18:54 -0400

    Add files via upload

--- face_detect.py
+++ face_detect.py
@@ -6,84 +6,138 @@ from tkinter import *
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
- #load xml for classifying faces
+# load xml for classifying faces
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml") 
 
-#title on tkinter GUI 
+# title on tkinter GUI
 root = tk.Tk()
+root.geometry("300x350")
+root.minsize(200, 200)
 root.title("Face Detect")
 
 # function to ask for photo
-def askDirect():
+def askPic():
     global filePath
     filePath = filedialog.askopenfilename()
+    print(filePath)
+
+# function to ask for directory to save known faces
+def askKnownDirec():
+    global directory
+    directory = filedialog.askdirectory()
+
+# submit button function
+def submit():
+    global name
+    name = name_var.get()
     
     
 # tkinter GUI for face detect
-askFile = Label(root, text = "Select a photo to face detect").grid(row=0, column=2)
-blank0 = Label(root, text = "            ").grid(row=0,column=1)
-blank1 = Label(root, text = "            ").grid(row=1,column=2)
-blank2 = Label(root, text = "            ").grid(row=2,column=2)
-blank3 = Label(root, text = "            ").grid(row=0,column=3)
+askFile = tk.Label(root, text="Select a photo to face detect", font=("Arial Black", 10))
+askFile.place(relx=0.15, rely=0.1, relheight=0.3, relwidth=0.7)
 
-#insert select photo button 
-select_button = Button(root, text = "Select a photo", command = askDirect)
-select_button.grid(row = 3, column = 2)
+# insert select photo button
+select_button = tk.Button(root, text="Select a photo", command=askPic, font=("Arial Black", 8 ))
+select_button.place(relx=0.30, rely=0.5, relheight=0.1, relwidth=0.35)
 
-#insert done button 
-done_button = Button(root, text = "Done", command = root.quit)
-done_button.grid(row = 4 , column = 2)
+# insert done button
+done_button = tk.Button(root, text="Done", command=root.destroy, font=("Arial Black", 8))
+done_button.place(relx=0.30, rely=0.65, relheight=0.1, relwidth=0.35)
 
-#run GUI 
+# run GUI
 root.mainloop()
 
-#print file path 
-print("The file you have selected is: " + filePath)
+# read the image
+img = cv2.imread(filePath)
 
+# convert image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-out_img = filePath
-
-#read the image
-img = cv2.imread(out_img) 
-#convert image to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-
-#detect faces 
+# detect faces
 faces = face_cascade.detectMultiScale(gray, 1.1, 6)
 
-#draw rectangle around face
+# draw rectangle around face
 for(x,y,w,h) in faces:
-    img =cv2.rectangle(img,(x,y),(x+w,y+h),(2,33,233),7) 
+    img = cv2.rectangle(img, (x, y), (x+w, y+h), (2, 33, 233), 3)
     roi_gray = gray[y:y+h, x:x+w] 
-    roi_color = img[y:y+h, x:x+w] 
+    roi_color = img[y:y+h, x:x+w]
 
-#show image with all faces detected 
-cv2.imshow('img', img) 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# tkinter window for selecting directory to save known faces
+root2 = tk.Tk()
+root2.geometry("300x350")
+root2.minsize(400,400)
+root2.title("Select Known Faces Directory")
 
-#path for directory of known faces
-directory = r'C:\Users\William\3D Objects\Desktop\Python\known_faces' 
-directory_python = r'C:\Users\William\3D Objects\Desktop\Python'
+prompt = tk.Label(root2, text="Select a directory to store tagged faces:", font=("Arial Black", 10))
+prompt.place(relx=0.1, rely=0.25, relheight=0.3, relwidth=0.85)
 
-#change directory to known faces
-os.chdir(directory) 
+select_button2 = tk.Button(root2, text="Select a directory", command=askKnownDirec, font=("Arial Black", 8 ))
+select_button2.place(relx=0.30, rely=0.5, relheight=0.1, relwidth=0.35)
 
- # crop the image with a single face and show cropped image 
+done_button2 = tk.Button(root2, text="Done", command=root2.destroy, font=("Arial Black", 8))
+done_button2.place(relx=0.30, rely=0.65, relheight=0.1, relwidth=0.35)
+
+root2.mainloop()
+
+# change directory to known faces and if not created, create directory for detected faces
+os.chdir(directory)
+path1 = os.path.join(directory, "detectedFaces")
+if not os.path.exists(path1):
+    os.mkdir(path1)
+
+# write detected faces image to directory
+os.chdir(path1)
+cv2.imwrite("detected.jpg", img)
+
+
+# tkinter window for displaying face detected image
+root3 = tk.Tk()
+root3.minsize(400, 400)
+root3.geometry("600x400")
+root3.title("Detected Faces")
+
+canvas = tk.Canvas(root3, width=500, height=300)
+canvas.pack()
+canvasImg = ImageTk.PhotoImage(Image.open("detected.jpg"))
+canvas.create_image(0, 0, anchor="nw", image=canvasImg)
+
+done_button3 = tk.Button(root3, text="Done", command=root3.destroy, font=("Arial Black", 8))
+done_button3.place(relx=0.33, rely=0.78, relwidth=0.3, relheigh=0.15)
+root3.mainloop()
+
+
+
+# tkinter windows to display each detected face and prompt for name entry from the user
 for(x,y,w,h) in faces:
-    crop_image = img[y:y+h, x:x+w] 
-    cv2.imshow("Cropped", crop_image) 
-    cv2.waitKey(1)
+    os.chdir(path1)
+    root4 = tk.Tk()
+    root4.minsize(200, 300)
+    root4.geometry("300x300")
+    root4.title("Tag Face")
 
-    # ask for user to input the name of person in image 
-    name = input("Who is in this photo? ") 
+    canvas2 = tk.Canvas(root4)
+    canvas2.place(relx=0.4, rely=0.35, relwidth=0.5, relheight=0.5)
 
-    path = os.path.join(directory, name) #create new directory path name
-    os.mkdir(path) # create new directory 
-    os.chdir(path) # change path to newly created directory 
+    crop_image = img[y:y+h, x:x+w]
+    cv2.imwrite("face.jpg", crop_image)
+    canvasImg2 = ImageTk.PhotoImage(Image.open("face.jpg"))
+    canvas2.create_image(0, 0, anchor="nw", image=canvasImg2)
 
-    # save cropped image as the name of the person in a unique directory
-    cv2.imwrite(name + ".jpg",crop_image) 
-    cv2.waitKey(1)
+    name_label = tk.Label(root4, text="Enter a name: ")
+    name_label.place(relx=0.05, rely=0.7, relheight=0.1, relwidth=0.35)
 
+    name_var = tk.StringVar()
+    name_entry = tk.Entry(root4, textvariable=name_var)
+    name_entry.place(relx=0.45, rely=0.73, relheight=0.05, relwidth=0.5)
+
+    submit = tk.Button(root4, text="Submit", command=root4.destroy)
+    submit.place(relx=0.35, rely=0.85, relheight=0.05, relwidth=0.35)
+    root4.mainloop()
+
+    # write tagged face image to its own directory
+    path2 = os.path.join(directory, name_var.get())
+    os.mkdir(path2)
+    os.chdir(path2)
+
+    cv2.imwrite(name_var.get() + ".jpg", crop_image)
 
--- face_recog.py
+++ face_recog.py
@@ -4,84 +4,69 @@ import face_recognition
 import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
-from PIL import Image, ImageTk
 
-known_faces_direc = "known_faces" #directory for known faces
-unknown_faces_direc = "unknown_faces" #directory of unknown faces
-tolerance = 0.9 #tolerance for matching criteria
-frame_thickness = 3 #pixels
-font_thinckness = 2 
-model = "hog" #hisotgram of oriented gradients 
-
-print("Loading known faces")
-
-known_faces = [] #array of known faces
-known_names = [] #array of known names
-
-
-
-for name in os.listdir(known_faces_direc): #iterate through known faces directory 
-    for filename in os.listdir(f"{known_faces_direc}/{name}"): #iterate through images in directory 
-        image = face_recognition.load_image_file(f"{known_faces_direc}/{name}/{filename}") #load the image file  
-        encoding = face_recognition.face_encodings(image)[0] #encode loaded image 
-        known_faces.append(encoding) #appends face encoding to array of known faces
-        known_names.append(name) # appends name to array of known names
-
-print("Processing unkown faces")
-
-root = tk.Tk() #start tkinter gui 
-root.title("Face Recognition")
-
-def askDirect(): # ask user to select directory 
+# ask user to select directory
+def askDirect():
     global filePath
     filePath = filedialog.askdirectory()
-    
-    
-# tkinter gui to select a directory 
-askFile = Label(root, text = "Select a directory to find faces").grid(row=0, column=2)
-blank0 = Label(root, text = "            ").grid(row=0,column=1)
-blank1 = Label(root, text = "            ").grid(row=1,column=2)
-blank2 = Label(root, text = "            ").grid(row=2,column=2)
-blank3 = Label(root, text = "            ").grid(row=0,column=3)
 
-#insert select directory button 
-select_button = Button(root, text = "Select a directory", command = askDirect)
-select_button.grid(row = 3, column = 2)
 
-#insert done button 
-done_button = Button(root, text = "Done", command = root.quit)
-done_button.grid(row = 4 , column = 2)
+known_faces_direc = "known_faces" #directory for known faces
+tolerance = 0.65 #tolerance for matching criteria
+model = "hog" #hisotgram of oriented gradients 
 
-#run GUI 
+
+known_faces = []
+known_names = []
+
+
+# iterate through the known faces directory and grab the face encodings and names to store in arrays declared above
+for name in os.listdir(known_faces_direc):
+    for filename in os.listdir(f"{known_faces_direc}/{name}"):
+        image = face_recognition.load_image_file(f"{known_faces_direc}/{name}/{filename}")
+        encoding = face_recognition.face_encodings(image)[0]
+        known_faces.append(encoding)
+        known_names.append(name)
+
+
+# tkinter window for photo directory selection
+root = tk.Tk()
+root.geometry("250x350")
+root.minsize(200, 200)
+root.title("Face Recognition")
+
+askFile = tk.Label(root, text="Select a directory to find faces")
+askFile.place(relx=0.1, rely=0.2, relwidth=0.8, relheight=0.25)
+
+select_button = tk.Button(root, text="Select a directory", command=askDirect)
+select_button.place(relx=0.35, rely=0.7, relwidth=0.5, relheight=0.15)
+
+done_button = tk.Button(root, text="Done", comman=root.quit)
+done_button.place(relx=0.35, rely=0.8, relheight=0.15, relwidth=0.5)
+
 root.mainloop()
-print("The file you have selected is: " + filePath)
 
-unknown_faces_direc = filePath
+# iterate through unkown faces images, locate the faces in a picture & grab facial encodings
+for filename in os.listdir(filePath):
+    image = face_recognition.load_image_file(f"{filePath}/{filename}")
+    locations = face_recognition.face_locations(image, model=model)
+    encodings = face_recognition.face_encodings(image, locations)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-for filename in os.listdir(unknown_faces_direc): #iterate through unkown faces directory
-    print(filename)
-    image = face_recognition.load_image_file(f"{unknown_faces_direc}/{filename}") #load unknown image
-    locations = face_recognition.face_locations(image, model = model) #locate faces 
-    encodings = face_recognition.face_encodings(image, locations) #grab face encodings
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) #convert image from rgb to bgr 
-
-    for face_encoding, face_location in zip(encodings, locations): #iterate through locations and encodings of unknowkn faces 
-        results = face_recognition.compare_faces(known_faces, face_encoding, tolerance) #compare unkown encodings to known faces 
+    # for every found face compare the encodings to known faces with matching tolerance incorporated
+    for face_encoding, face_location in zip(encodings, locations):
+        results = face_recognition.compare_faces(known_faces, face_encoding, tolerance)
         match = None
-        if True in results: #if there is a match print the match found 
+        if True in results:
             match = known_names[results.index(True)]
-            print(f"Match found: {match}")
 
             #draw rectangle over face
-            #find top left and bottom right of face 
             top_left = (face_location[3], face_location[0])
             bottom_right = (face_location[1], face_location[2])
 
-            #color of rectangle
-            color = [255,255,255]
+            color = [0, 0, 0]
 
-            #draw rectangle 
-            cv2.rectangle(image, top_left, bottom_right, color, frame_thickness)
+            cv2.rectangle(image, top_left, bottom_right, color, 3)
 
             #location to put name
             top_left = (face_location[3], face_location[2])
@@ -89,7 +74,7 @@ for filename in os.listdir(unknown_faces_direc): #iterate through unkown faces d
 
             #draw rectangle and display name 
             cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
-            cv2.putText(image, match, (face_location[3]+10,face_location[2]+15), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,0,0), font_thinckness)
+            cv2.putText(image, match, (face_location[3]+10,face_location[2]+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
    
     #display image
     cv2.imshow(filename, image)
